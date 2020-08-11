ba26fcfda8dd33f5cd06ebecd15c5ded073e304b
uttambhandari91 <bhandari91uttam@gmail.com>
2020-08-11 00:04:25 -0500

    This is my base code

---
+++ src/main/java/com/test/HelloController.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class HelloController {
+
+	@GetMapping ("/test")
+	public String test() {
+	return "test";
+}
+}
